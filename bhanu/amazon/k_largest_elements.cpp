b28860244ff6b7d9d99dea2bd85f87e19e82fa49
Bhanu Prakash <bhanuprakash.p@xseededucation.com>
2020-10-12 00:40:12 +0530

    fix review comments

--- bhanu/amazon/k_largest_elements.cpp
+++ bhanu/amazon/k_largest_elements.cpp
@@ -2,13 +2,21 @@
 #include <priority_queue>
 using namespace std;
 
-solve(vector<int> &A, int B) {
-    priority_queue<int> Q;
-    for (auto i : A) {
-        Q.push(i);
+vector<int> solve(vector<int> &A, int B) {
+    // priority_queue<int> Q;
+    priority_queue <int, vector<int>, greater<int> > Q;
+    for (int i = 0; i < B; i++) {
+        Q.push(A[i]);
+    }
+    
+    for (int i = B; i < A.size(); i++) {
+        if (Q.top() < A[i]) {
+            Q.pop();
+            Q.push(A[i]);
+        }
     }
     vector<int> res;
-    for (int i = 0; i < B; i++) {
+    while(!Q.empty()) {
         res.push_back(Q.top());
         Q.pop();
     }
